7324f5625dba7fbef2a28fea87c37a268cf75159
RSSchermer <roland0507@gmail.com>
2020-02-06 23:55:52 +0100

    Fix array initializer

--- std140/src/lib.rs
+++ std140/src/lib.rs
@@ -234,7 +234,7 @@ where
 #[macro_export]
 macro_rules! array {
     ($elem:expr; $n:expr) => {
-        $crate::array::from_wrapped([$crate::std140::ArrayElementWrapper {
+        $crate::array::from_wrapped([$crate::ArrayElementWrapper {
             element: $elem
         }; $n])
     };
